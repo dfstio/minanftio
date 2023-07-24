2d89ac616013eb789f95f91e19b75a416d66e941
Benjamin Herald <bherald@pm.me>
2023-07-24 19:26:55 +0300

    stripe2

--- src/routes/Create/MintMenu.js
+++ src/routes/Create/MintMenu.js
@@ -7,11 +7,24 @@ const {Meta} = Card;
 const MintMenuItem = ({ title, creator, description, price, link, image }) => {
   return (
     <div className="gx-product-item gx-product-vertical" >
+    {(link === "")?(
+     <Card
+        title={creator}
+        cover={<img alt="example" src={image}/>}
+        bordered={false}
+        >
+      <div className="gx-product-name">
+      <p>{description}</p>
+        <Meta
+        title={title}
+        description={price}
+      />
+      </div>
+    </Card>   
+    ):(
     <Card
         title={creator}
-          {(link !== '')?(
         extra={ <a href={link}><span className="gx-link">Create</span></a>}
-        )}
         cover={<img alt="example" src={image}/>}
         bordered={false}
         >
@@ -23,6 +36,7 @@ const MintMenuItem = ({ title, creator, description, price, link, image }) => {
       />
       </div>
     </Card>
+    )
     </div>
 
   );
