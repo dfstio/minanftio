6426d9d4af9cc78798cd74c9f49282818597e7c5
Benjamin Herald <bherald@pm.me>
2023-07-24 17:53:00 +0300

    menu2

---
+++ src/routes/Create/MintMenu.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {Card} from "antd";
+
+const {Meta} = Card;
+
+
+const MintMenuItem = ({ title, creator, description, price, link, image }) => {
+  return (
+    <div className="gx-product-item gx-product-vertical" >
+    <Card
+        title={creator}
+        extra={ <a href={link}><span className="gx-link">Create</span></a>}
+        cover={<img alt="example" src={image}/>}
+        bordered={false}
+        >
+      <div className="gx-product-name">
+      <p>{description}</p>
+        <Meta
+        title={title}
+        description={price}
+      />
+      </div>
+    </Card>
+    </div>
+
+  );
+};
+
+export default MintMenuItem;
+
---
+++ src/routes/Posts/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {Button} from "antd";
+import {connectSearchBox, SortBy} from "react-instantsearch-dom";
+
+
+const AlgoliaHeader = ({onCloseFunction}) => (
+  <div className="gx-algolia-header">
+                   <span className="gx-drawer-btn gx-d-flex gx-d-lg-none" >
+                  <Button
+                    type="link"
+                    onClick={onCloseFunction}
+                    >
+                    Filter
+                    </Button>
+        </span>
+    <ConnectedSearchBox/>
+    <div className="gx-algolia-sort-by">
+      <label>Sort by</label>
+      <SortBy
+        items={[
+          {value: 'tokenId', label: 'Creation date'},
+          {value: 'virtuoso', label: 'Sale date'},
+        ]}
+        defaultRefinement="minanft"
+      />
+    </div>
+  </div>
+);
+const CustomSearchBox = ({currentRefinement, refine}) => (
+  <div className="gx-search-bar gx-lt-icon-search-bar">
+    <div className="gx-form-group">
+      <input
+        type="search"
+        placeholder="Search here..."
+        value={currentRefinement}
+        onChange={e => refine(e.target.value)}
+        autoComplete="off"
+        className="ant-input form-control"
+        id="q"
+      />
+      <span className="gx-search-icon gx-pointer"><i className="icon icon-search"/></span>
+    </div>
+  </div>
+);
+const ConnectedSearchBox = connectSearchBox(CustomSearchBox);
+
+export default AlgoliaHeader;
