b2cc98b4526f177f012a5b6e83accaef0d33d901
Benjamin Herald <bherald@pm.me>
2023-07-25 20:52:58 +0300

    google3

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TagManager from 'react-gtm-module'
+//import TagManager from 'react-gtm-module'
 import NextApp from './NextApp';
 import * as serviceWorker from './registerServiceWorker';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
+/*
 const tagManagerArgs = {
     gtmId: 'G-K4Q0KRCMVX' //'GTM-NQS8XXKH'
 }
 TagManager.initialize(tagManagerArgs);
+*/
+
 ReactDOM.render(<NextApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
