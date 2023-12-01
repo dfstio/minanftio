0de95b53604532174a0e08095cb5f5944dbc16f6
DFST <mk@dfst.io>
2023-12-01 16:22:32 +0400

    JWT token api

--- src/serverless/botlambda.js
+++ src/serverless/botlambda.js
@@ -4,25 +4,26 @@ const axios = require("axios");
 
 const { BOTAPIAUTH, BOTAPIURL } = process.env;
 
-async function lambda(command, data) {
-    const lambdaData = {
-        auth: BOTAPIAUTH,
-        command,
-        data,
-    };
-    console.log("functionsLambdaBot command", command, data);
+async function lambda(command, jwtToken, data) {
+  const lambdaData = {
+    auth: BOTAPIAUTH,
+    command,
+    jwtToken,
+    data,
+  };
+  console.log("functionsLambdaBot command", command, data);
 
-    try {
-        const response = await axios.post(`${BOTAPIURL}`, lambdaData);
-        //console.log("functionsLambdaBot result", data, response);
-        return { success: true };
-    } catch (error) {
-        logm.error("catch", { error, data });
-        console.error("catch functionsLambdaBot", data, error);
-        return { success: false };
-    }
+  try {
+    const response = await axios.post(`${BOTAPIURL}`, lambdaData);
+    //console.log("functionsLambdaBot result", data, response);
+    return { success: true };
+  } catch (error) {
+    logm.error("catch", { error, data });
+    console.error("catch functionsLambdaBot", data, error);
+    return { success: false };
+  }
 }
 
 module.exports = {
-    lambda,
+  lambda,
 };
