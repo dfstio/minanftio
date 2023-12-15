65729538878d663be280dd360757f36bcea930e5
DFST <mk@dfst.io>
2023-12-15 23:13:25 +0400

    less

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const {
   override,
-  addLessLoader,
+  //addLessLoader,
   addWebpackAlias,
   addBabelPlugin,
   addWebpackExternals,
 } = require("customize-cra");
+const addLessLoader = require("customize-cra-less-loader");
 
 const overrideProcessEnv = (value) => (config) => {
   config.resolve.modules = [path.join(__dirname, "src")].concat(
@@ -14,33 +15,69 @@ const overrideProcessEnv = (value) => (config) => {
   return config;
 };
 
+const addWebpackAwait = () => (config) => {
+  config.experiments = { ...config.experiments, topLevelAwait: true };
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    os: false,
+    fs: false,
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    path: require.resolve("path-browserify"),
+    stream: require.resolve("stream-browserify"),
+    zlib: require.resolve("browserify-zlib"),
+    assert: require.resolve("assert/"),
+    crypto: require.resolve("crypto-browserify"),
+  };
+  return config;
+};
+
 module.exports = override(
+  /*
   addLessLoader({
     javascriptEnabled: true,
     modifyVars: { "@primary-color": "#038fde" },
   }),
+  */
+  addLessLoader({
+    lessLoaderOptions: {
+      javascriptEnabled: true,
+      lessOptions: {
+        javascriptEnabled: true,
+        modifyVars: {
+          "@primary-color": "#038fde",
+        },
+      },
+    },
+  }),
   overrideProcessEnv({
     VERSION: JSON.stringify(require("./package.json").version),
   }),
+
   /*
   addWebpackAlias({
     ["o1js"]: path.resolve(__dirname, "node_modules/o1js"),
   }),
   */
-  addBabelPlugin("@babel/plugin-syntax-top-level-await"),
+
+  addWebpackAwait(),
+
+  addBabelPlugin("@babel/plugin-syntax-top-level-await")
+  /*
   addWebpackExternals({
     o1js: "o1js",
   })
+  */
 );
 
 /*
-  webpack: function (config, env) {
+  webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
-      o1js: require("path").resolve("node_modules/o1js"),
+      o1js: require('path').resolve('node_modules/o1js'),
     };
-    config = { ...config, topLevelAwait: true };
+    config.experiments = { ...config.experiments, topLevelAwait: true };
     config.optimization.minimizer = [];
     return config;
   },
-  */
+*/
