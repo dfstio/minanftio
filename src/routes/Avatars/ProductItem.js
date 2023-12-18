1af3b8f7bd225602bbb462a4f4e203fb87cad073
DFST <mk@dfst.io>
2023-12-18 20:47:17 +0400

    v16

--- src/routes/Avatars/ProductItem.js
+++ src/routes/Avatars/ProductItem.js
@@ -11,45 +11,46 @@ import BuyButton from "./Buy";
 const DEBUG = "true" === process.env.REACT_APP_DEBUG;
 
 const ProductItem = ({ item }) => {
-    //const icons = [];
-    console.log("Item: ", item);
-    const address = useSelector(({ blockchain }) => blockchain.address);
-    const dispatch = useDispatch();
-    let buttonId = "sidebar.algolia.buy";
-    let canSell = false;
-    if (address.toUpperCase() === item.owner.toUpperCase()) {
-        buttonId = "sidebar.algolia.sell";
-        canSell = true;
-    }
-    const tokenPath = item.tokenId;
+  //const icons = [];
+  console.log("Item: ", item);
+  const address = useSelector(({ blockchain }) => blockchain.address);
+  const dispatch = useDispatch();
+  let buttonId = "sidebar.algolia.buy";
+  let canSell = false;
+  if (address.toUpperCase() === item.owner.toUpperCase()) {
+    buttonId = "sidebar.algolia.sell";
+    canSell = true;
+  }
+  const tokenPath = item.tokenId;
 
-    return (
-        <div className="gx-product-item gx-product-vertical">
-            <a href={tokenPath}>
-                <div className="gx-product-image">
-                    <img
-                        src={`https://res.cloudinary.com/minanft/image/fetch/h_300,q_100,f_auto/${item.image}`}
-                        alt=""
-                    />
-                </div>
-            </a>
-            <div className="gx-product-body">
-                <div className="gx-product-name">
-                    <a href={tokenPath}>
-                        <span>
-                            <Highlight attribute="name" hit={item} />
-                        </span>
-                    </a>
-                    {canSell ? (
-                        <span style={{ float: "right" }}>
-                            <SellButton item={item} address={address} />
-                        </span>
-                    ) : (
-                        <span>
-                            {item.onSale ? (
-                                <span style={{ float: "right" }}>
-                                    <BuyButton item={item} />
-                                    {/*
+  return (
+    <div className="gx-product-item gx-product-vertical">
+      <a href={tokenPath}>
+        <div className="gx-product-image">
+          <img
+            src={`https://res.cloudinary.com/minanft/image/fetch/h_300,q_100,f_auto/${item.image}`}
+            alt=""
+            crossorigin
+          />
+        </div>
+      </a>
+      <div className="gx-product-body">
+        <div className="gx-product-name">
+          <a href={tokenPath}>
+            <span>
+              <Highlight attribute="name" hit={item} />
+            </span>
+          </a>
+          {canSell ? (
+            <span style={{ float: "right" }}>
+              <SellButton item={item} address={address} />
+            </span>
+          ) : (
+            <span>
+              {item.onSale ? (
+                <span style={{ float: "right" }}>
+                  <BuyButton item={item} />
+                  {/*
           <Button
           type="primary"
           onClick={ async () => {
@@ -90,39 +91,36 @@ const ProductItem = ({ item }) => {
             <IntlMessages id={buttonId}/>
             </Button>
           */}
-                                </span>
-                            ) : (
-                                ""
-                            )}
-                        </span>
-                    )}
-                </div>
-                <div className="gx-mb-3">
-                    <Highlight attribute="category" hit={item} />
-                </div>
-
-                {item.onSale ? (
-                    <div className="gx-product-price">
-                        <span style={{ float: "right" }}>
-                            {item.currency} {item.price}
-                        </span>
-                    </div>
-                ) : (
-                    ""
-                )}
-                <div className="gx-mt-4" style={{ whiteSpace: "pre-wrap" }}>
-                    <a href={tokenPath}>
-                        <span>
-                            <Highlight
-                                attribute="shortdescription"
-                                hit={item}
-                            />
-                        </span>
-                    </a>
-                </div>
-            </div>
+                </span>
+              ) : (
+                ""
+              )}
+            </span>
+          )}
         </div>
-    );
+        <div className="gx-mb-3">
+          <Highlight attribute="category" hit={item} />
+        </div>
+
+        {item.onSale ? (
+          <div className="gx-product-price">
+            <span style={{ float: "right" }}>
+              {item.currency} {item.price}
+            </span>
+          </div>
+        ) : (
+          ""
+        )}
+        <div className="gx-mt-4" style={{ whiteSpace: "pre-wrap" }}>
+          <a href={tokenPath}>
+            <span>
+              <Highlight attribute="shortdescription" hit={item} />
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductItem;
