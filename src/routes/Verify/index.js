cfb997e262f050093573fdbbc5ad172b13b828f1
DFST <mk@dfst.io>
2023-12-19 14:31:18 +0400

    v12

--- src/routes/Verify/index.js
+++ src/routes/Verify/index.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateAddress, updatePublicKey } from "../../appRedux/actions";
 import { minaLogin } from "../../blockchain/mina";
 import { Field, fetchAccount, PublicKey, Mina, SmartContract } from "o1js";
-import { makeString } from "minanft";
+import minanft from "minanft";
 
 import IntlMessages from "util/IntlMessages";
 
@@ -118,7 +118,7 @@ const Verify = () => {
     */
     //const balanceMina = await accountBalanceMina(publicKey);
     //const { makeString } = await import("minanft");
-    const balanceMina = makeString(12);
+    const balanceMina = minanft.makeString(12);
     console.log("balanceMina", balanceMina);
   }
 
