bf3631008a983866cf90078f0446d8418ecabb40
DFST <mk@dfst.io>
2023-12-19 13:53:26 +0400

    v5

--- config-overrides.js
+++ config-overrides.js
@@ -106,10 +106,9 @@ module.exports = override(
 
   addWebpackAwait(),
 
-  addBabelPlugin("@babel/plugin-syntax-top-level-await")
-  /*
+  addBabelPlugin("@babel/plugin-syntax-top-level-await"),
+
   addWebpackExternals({
     o1js: "o1js",
   })
-*/
 );
