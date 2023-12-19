28efbb00dcbaf18eec988612b56b9ad8577df9a9
DFST <mk@dfst.io>
2023-12-19 17:48:48 +0400

    v3

--- src/routes/Verify/index.js
+++ src/routes/Verify/index.js
@@ -3,8 +3,23 @@ import { Button, message } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { updateAddress, updatePublicKey } from "../../appRedux/actions";
 import { minaLogin } from "../../blockchain/mina";
-import { Field, fetchAccount, PublicKey, Mina, SmartContract } from "o1js";
-import { makeString, accountBalanceMina } from "minanft";
+import {
+  Field,
+  fetchAccount,
+  PublicKey,
+  Mina,
+  SmartContract,
+  PrivateKey,
+  Poseidon,
+} from "o1js";
+import {
+  MinaNFT,
+  MapData,
+  MinaNFTNameService,
+  MINANFT_NAME_SERVICE,
+  accountBalanceMina,
+  makeString,
+} from "minanft";
 
 import IntlMessages from "util/IntlMessages";
 
@@ -82,17 +97,112 @@ const Verify = () => {
     }
   }
 
+  async function mintNFT() {
+    const DEPLOYER = "";
+    const NAMES_ORACLE_SK = "";
+    const PINATA_JWT = "";
+    const keys = MinaNFT.minaInit("local");
+    const deployer = keys
+      ? keys[0].privateKey
+      : PrivateKey.fromBase58(DEPLOYER);
+    const oraclePrivateKey = keys
+      ? PrivateKey.random()
+      : PrivateKey.fromBase58(NAMES_ORACLE_SK);
+    //const nameServiceAddress = PublicKey.fromBase58(MINANFT_NAME_SERVICE);
+
+    const ownerPrivateKey = PrivateKey.random();
+    const ownerPublicKey = ownerPrivateKey.toPublicKey();
+    const owner = Poseidon.hash(ownerPublicKey.toFields());
+    const pinataJWT = PINATA_JWT;
+
+    console.log(
+      `Deployer balance: ${await accountBalanceMina(deployer.toPublicKey())}`
+    );
+
+    const nft = new MinaNFT({ name: `@test` + makeString(10) });
+    nft.updateText({
+      key: `description`,
+      text: "This is my long description of the NFT. Can be of any length, supports markdown.",
+    });
+    nft.update({ key: `twitter`, value: `@builder` });
+    nft.update({ key: `secret`, value: `mysecretvalue`, isPrivate: true });
+
+    /*
+    await nft.updateImage({
+      filename: "./images/navigator.jpg",
+      pinataJWT,
+    });
+  
+    const map = new MapData();
+    map.update({ key: `level2-1`, value: `value21` });
+    map.update({ key: `level2-2`, value: `value22` });
+    map.updateText({
+      key: `level2-3`,
+      text: `This is text on level 2. Can be very long`,
+    });
+  
+    await map.updateFile({
+      key: "woman",
+      filename: "./images/woman.png",
+      pinataJWT,
+    });
+  
+    
+  
+    const mapLevel3 = new MapData();
+    mapLevel3.update({ key: `level3-1`, value: `value31` });
+    mapLevel3.update({ key: `level3-2`, value: `value32`, isPrivate: true });
+    mapLevel3.update({ key: `level3-3`, value: `value33` });
+    map.updateMap({ key: `level2-4`, map: mapLevel3 });
+    nft.updateMap({ key: `level 2 and 3 data`, map });
+    */
+
+    console.log(`json:`, JSON.stringify(nft.toJSON(), null, 2));
+    console.log("Compiling...");
+    await MinaNFT.compile();
+
+    const nameService = new MinaNFTNameService({ oraclePrivateKey });
+    let tx = await nameService.deploy(deployer);
+    if (tx === undefined) {
+      throw new Error("Deploy failed");
+    }
+    await MinaNFT.wait(tx);
+
+    /*
+    const nameService = new MinaNFTNameService({
+      oraclePrivateKey,
+      address: nameServiceAddress,
+    });
+    
+    */
+    tx = await nft.mint({
+      deployer,
+      owner,
+      pinataJWT,
+      nameService,
+    });
+    if (tx === undefined) {
+      throw new Error("Mint failed");
+    }
+    console.log("Waiting for transaction to be included in a block...");
+    console.time("Transaction included in a block");
+    await MinaNFT.wait(tx);
+    console.timeEnd("Transaction included in a block");
+  }
+
   async function connect() {
     log.info("Connect clicked", { address, wf: "connect" });
 
+    const newAddress = await minaLogin();
+    console.log("newAddress", newAddress);
+    dispatch(updateAddress(newAddress));
+
+    /*
     const network = Mina.Network({
       mina: "https://proxy.testworld.minaexplorer.com/graphql",
     });
 
     Mina.setActiveInstance(network);
-    const newAddress = await minaLogin();
-    console.log("newAddress", newAddress);
-    dispatch(updateAddress(newAddress));
 
     const a = Field(7);
     const b = Field(3);
@@ -114,6 +224,9 @@ const Verify = () => {
 
     const balanceMina = await accountBalanceMina(publicKey);
     console.log("balanceMina", balanceMina, makeString(12));
+
+    */
+    await mintNFT();
   }
 
   return (
