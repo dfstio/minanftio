5260792261be26f9d2642b0cd1882dc5815a7952
DFST <mk@dfst.io>
2023-12-22 20:50:42 +0400

    billing

--- src/routes/CorporatePortal/index.js
+++ src/routes/CorporatePortal/index.js
@@ -38,12 +38,24 @@ const Mint = () => {
 
         <Col xxl={8} xl={8} lg={12} md={12} sm={24} xs={24}>
           <MintMenuItem
+            creator=<IntlMessages id="corporate.billing" />
+            title=<IntlMessages id="corporate.billingtitle" />
+            link="/corporate/kyc"
+            button=<IntlMessages id="corporate.billingbutton" />
+            description=<IntlMessages id="corporate.billingdescription" />
+            image="https://res.cloudinary.com/minanft/image/fetch/h_300,q_100,f_auto/https://minanft-storage.s3.eu-west-1.amazonaws.com/CorporateBilling.png"
+            key="Create Corporate Account"
+          />
+        </Col>
+
+        <Col xxl={8} xl={8} lg={12} md={12} sm={24} xs={24}>
+          <MintMenuItem
             creator="Description of the Mina NFT project"
             title="Description of Mina NFT project"
             link=""
             button="Read More"
             description="MinaNFT is an innovative NFT project leveraging the Mina blockchain's unique privacy features and AI technology. Our platform allows users to create personalised avatar NFTs and use them as an identity symbol across various social media. By interacting with our Telegram bot, users can describe avatar idea by texting or sending voice message in any language, and our AI will generate a unique NFT. Additionally, our avatar NFTs are equipped to host verifiable proofs of authenticity. Users can securely attach and share public and private sensitive content such as art, contracts, medical records, or ownership proofs, transforming traditional NFTs into versatile digital identities. Individuals and businesses are welcome to join MinaNFT, a space where art meets privacy, and personalize their digital footprint"
-            image="https://res.cloudinary.com/minanft/image/fetch/h_300,q_100,f_auto/https://content.nftvirtuoso.io/image/mintimages/deepnft.jpg"
+            image="https://res.cloudinary.com/minanft/image/fetch/h_300,q_100,f_auto/https://minanft-storage.s3.eu-west-1.amazonaws.com/CorporateDocs.png"
             key="MinaNFT Description"
           />
         </Col>
