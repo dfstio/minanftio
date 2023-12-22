441bca87046687e0b097e577722a49284de95251
DFST <mk@dfst.io>
2023-12-22 23:26:07 +0400

    pay

--- src/routes/CorporateBilling/billing.js
+++ src/routes/CorporateBilling/billing.js
@@ -8,5 +8,36 @@ export async function queryBilling(auth) {
   const minanft = new api(JWT);
   const report = await minanft.queryBilling();
   console.log("queryBilling result", report);
+
+  /*
+billedDuration
+: 
+141818
+developer
+: 
+"@dfst"
+id
+: 
+"6459034946"
+jobId
+: 
+"6459034946.1703085063711.s1ejoc02ff4e19vfb5s6wyp25mm1615x"
+jobName
+: 
+"mint"
+jobStatus
+: 
+"used"
+task
+: 
+"mint"
+timeCreated
+: 
+1703085063711
+timeFinished
+: 
+1703085208743
+  */
+
   return report;
 }
