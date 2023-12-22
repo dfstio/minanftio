ccb2d1e83fa8a446d2fcd3ba988b5c98dc2f0185
DFST <mk@dfst.io>
2023-12-22 17:58:34 +0400

    image

--- src/routes/Create/MintMenu.js
+++ src/routes/Create/MintMenu.js
@@ -4,38 +4,38 @@ import { Card } from "antd";
 const { Meta } = Card;
 
 const MintMenuItem = ({ title, creator, description, price, link, image }) => {
-    return (
-        <div className="gx-product-item gx-product-vertical">
-            {link === "" ? (
-                <Card
-                    title={creator}
-                    cover={<img alt="example" src={image} />}
-                    bordered={false}
-                >
-                    <div className="gx-product-name">
-                        <p>{description}</p>
-                        <Meta title={title} description={price} />
-                    </div>
-                </Card>
-            ) : (
-                <Card
-                    title={creator}
-                    extra={
-                        <a href={link}>
-                            <span className="gx-link">Create</span>
-                        </a>
-                    }
-                    cover={<img alt="example" src={image} />}
-                    bordered={false}
-                >
-                    <div className="gx-product-name">
-                        <p>{description}</p>
-                        <Meta title={title} description={price} />
-                    </div>
-                </Card>
-            )}
-        </div>
-    );
+  return (
+    <div className="gx-product-item gx-product-vertical">
+      {link === "" ? (
+        <Card
+          title={creator}
+          cover={<img alt="example" src={image} crossorigin="anonymous" />}
+          bordered={false}
+        >
+          <div className="gx-product-name">
+            <p>{description}</p>
+            <Meta title={title} description={price} />
+          </div>
+        </Card>
+      ) : (
+        <Card
+          title={creator}
+          extra={
+            <a href={link}>
+              <span className="gx-link">Create</span>
+            </a>
+          }
+          cover={<img alt="example" src={image} crossorigin="anonymous" />}
+          bordered={false}
+        >
+          <div className="gx-product-name">
+            <p>{description}</p>
+            <Meta title={title} description={price} />
+          </div>
+        </Card>
+      )}
+    </div>
+  );
 };
 
 export default MintMenuItem;
