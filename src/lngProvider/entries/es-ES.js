530b21b0a8f8392d8b1548d7e5ca183ff7fcee27
DFST <mk@dfst.io>
2023-12-27 18:33:57 +0400

    languages

---
+++ src/lngProvider/entries/es-ES.js
@@ -0,0 +1,11 @@
+import antdES from "antd/lib/locale-provider/es_ES";
+import esMessages from "../locales/es_ES.json";
+
+const esLang = {
+  messages: {
+    ...esMessages,
+  },
+  antd: antdES,
+  locale: "es-ES",
+};
+export default esLang;
