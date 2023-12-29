6a08b1c749738ac6bc4ff9d6657652d606f4f5ae
DFST <mk@dfst.io>
2023-12-29 22:17:31 +0400

    check

--- src/routes/VerifyAttributesOffChain/verify.js
+++ src/routes/VerifyAttributesOffChain/verify.js
@@ -1,10 +1,75 @@
 import { PublicKey } from "o1js";
-import { MinaNFT, RedactedMinaNFT, api, MINANFT_NAME_SERVICE } from "minanft";
+import {
+  MinaNFT,
+  RedactedMinaNFT,
+  api,
+  MINANFT_NAME_SERVICE,
+  MinaNFTContract,
+  MinaNFTNameServiceContract,
+} from "minanft";
+import { MerkleMap, fetchAccount } from "o1js";
 
 const { REACT_APP_JWT } = process.env;
 
+export async function check(json) {
+  MinaNFT.minaInit("testworld2");
+  let verified = true;
+  const data = new MerkleMap();
+  const kind = new MerkleMap();
+  for (const item in json.keys) {
+    data.set(
+      MinaNFT.stringToField(item),
+      MinaNFT.stringToField(json.keys[item])
+    );
+    kind.set(MinaNFT.stringToField(item), MinaNFT.stringToField("string"));
+  }
+  if (
+    data.getRoot().toJSON() !== json.proof?.publicInput[2] ||
+    kind.getRoot().toJSON() !== json.proof?.publicInput[3]
+  ) {
+    console.log(
+      "redacted metadata check error",
+      data.getRoot().toJSON(),
+      json.proof?.publicInput[2],
+      kind.getRoot().toJSON(),
+      json.proof?.publicInput[3]
+    );
+    verified = false;
+  }
+  const nameServiceAddress = PublicKey.fromBase58(MINANFT_NAME_SERVICE);
+  const zkNames = new MinaNFTNameServiceContract(nameServiceAddress);
+  const zkApp = new MinaNFTContract(
+    PublicKey.fromBase58(json.address),
+    zkNames.token.id
+  );
+  await fetchAccount({ publicKey: zkApp.address, tokenId: zkNames.token.id });
+  const metadata = zkApp.metadata.get();
+  if (
+    metadata.data.toJSON() !== json.proof?.publicInput[0] ||
+    metadata.kind.toJSON() !== json.proof?.publicInput[1]
+  ) {
+    console.log(
+      "metadata check error",
+      metadata.data.toJSON(),
+      json.proof?.publicInput[0],
+      metadata.kind.toJSON(),
+      json.proof?.publicInput[1]
+    );
+    verified = false;
+  }
+
+  return verified;
+}
+
 export async function verify(auth, json) {
   console.log("verify start", auth, json);
+  if ((await check(json)) === false) {
+    return {
+      success: false,
+      error: "JSON Verification error",
+      reason: "check failed",
+    };
+  }
   const JWT = auth === undefined || auth === "" ? REACT_APP_JWT : auth;
   const minanft = new api(JWT);
   const transactions = [JSON.stringify(json.proof)];
