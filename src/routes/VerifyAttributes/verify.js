209d75db3610b53717663c9e73e31b6fb7d42387
DFST <mk@dfst.io>
2023-12-29 20:13:18 +0400

    verify2

--- src/routes/VerifyAttributes/verify.js
+++ src/routes/VerifyAttributes/verify.js
@@ -3,47 +3,22 @@ import { MinaNFT, RedactedMinaNFT, api, MINANFT_NAME_SERVICE } from "minanft";
 
 const { REACT_APP_JWT } = process.env;
 
-export async function verify(auth, json, keys) {
-  console.log("prove start", auth, keys, json);
+export async function verify(auth, json) {
+  console.log("verify start", auth, json);
   const JWT = auth === undefined || auth === "" ? REACT_APP_JWT : auth;
   const minanft = new api(JWT);
-  const nameServiceAddress = PublicKey.fromBase58(MINANFT_NAME_SERVICE);
-  const blockchainInstance = "testworld2";
-  MinaNFT.minaInit(blockchainInstance);
-  console.log("nameServiceAddress", nameServiceAddress);
+  const transactions = [JSON.stringify(json.proof)];
 
-  const nft = new MinaNFT({
-    name: json.name,
-    address: PublicKey.fromBase58(json.address),
-    nameService: nameServiceAddress,
-  });
-
-  await nft.loadMetadata(JSON.stringify(json));
-  const loadedJson = nft.toJSON();
-  console.log(`loadedJson:`, JSON.stringify(loadedJson, null, 2));
-
-  const checkNft = await nft.checkState();
-  if (checkNft === false) {
-    console.error("NFT checkState error");
-    return;
-  }
-
-  const redactedNFT = new RedactedMinaNFT(nft);
-  for (const key of keys) {
-    console.log(`key:`, key);
-    redactedNFT.copyMetadata(key);
-  }
-  const transactions = await redactedNFT.prepareProofData();
   console.log("transactions", transactions.length);
   const result = await minanft.proof({
     transactions,
     developer: "@dfst",
     name: "map-proof",
-    task: "calculate",
+    task: "verify",
     args: [],
   });
 
-  console.log("proof job result", result);
+  console.log("verify job result", result);
 
   const jobId = result.jobId;
   if (jobId === undefined) {
