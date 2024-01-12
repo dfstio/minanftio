36330a0c5497312b2d01224d186c7e652707a057
DFST <mk@dfst.io>
2024-01-12 18:50:28 +0400

    sls7

--- src/serverless/logger.js
+++ src/serverless/logger.js
@@ -9,76 +9,76 @@ const { combine, timestamp, label, printf } = winston.format;
 var meta = { id: uuidv4(), type: "frontend", startTime: Date.now() };
 
 const myFormat = printf(({ level, message, winstonModule, wf, timestamp }) => {
-    return `${timestamp} ${level} [${winstonModule}:${wf}]: ${message}`;
+  return `${timestamp} ${level} [${winstonModule}:${wf}]: ${message}`;
 });
 
 class VirtuosoTransport extends Transport {
-    constructor(opts) {
-        super(opts);
-    }
+  constructor(opts) {
+    super(opts);
+  }
 
-    log(info, callback) {
-        setImmediate(() => {
-            api.winston(info);
-        });
+  log(info, callback) {
+    setImmediate(() => {
+      api.winston(info);
+    });
 
-        // Perform the writing to the remote service
-        callback();
-    }
+    // Perform the writing to the remote service
+    callback();
+  }
 }
 
 const transportInfo = [new VirtuosoTransport({ level: "info" })];
 const transportDebug = [new VirtuosoTransport({ level: "info" })];
 
 if (REACT_APP_DEBUG === "true") {
-    transportInfo.push(
-        new winston.transports.Console({
-            colorize: true,
-            timestamp: true,
-            level: "info",
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.timestamp({ format: "HH:mm:ss.SSS" }),
-                myFormat,
-            ),
-        }),
-    );
-    transportDebug.push(
-        new winston.transports.Console({
-            colorize: true,
-            timestamp: true,
-            level: "debug",
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.timestamp({ format: "HH:mm:ss.SSS" }),
-                myFormat,
-            ),
-        }),
-    );
+  transportInfo.push(
+    new winston.transports.Console({
+      colorize: true,
+      timestamp: true,
+      level: "info",
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: "HH:mm:ss.SSS" }),
+        myFormat
+      ),
+    })
+  );
+  transportDebug.push(
+    new winston.transports.Console({
+      colorize: true,
+      timestamp: true,
+      level: "debug",
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: "HH:mm:ss.SSS" }),
+        myFormat
+      ),
+    })
+  );
 }
 
 const info = new winston.createLogger({
-    level: "info",
-    format: winston.format.json(),
-    transports: transportInfo,
-    defaultMeta: { winstonFrontendMeta: meta },
-    exceptionHandlers: transportInfo,
-    rejectionHandlers: transportInfo,
-    exitOnError: false,
+  level: "info",
+  format: winston.format.json(),
+  transports: transportInfo,
+  defaultMeta: { winstonFrontendMeta: meta },
+  exceptionHandlers: transportInfo,
+  rejectionHandlers: transportInfo,
+  exitOnError: false,
 });
 
 const debug = new winston.createLogger({
-    level: "debug",
-    format: winston.format.json(),
-    transports: transportDebug,
-    defaultMeta: { winstonFrontendMeta: meta },
-    exceptionHandlers: transportDebug,
-    rejectionHandlers: transportDebug,
-    exitOnError: false,
+  level: "debug",
+  format: winston.format.json(),
+  transports: transportDebug,
+  defaultMeta: { winstonFrontendMeta: meta },
+  exceptionHandlers: transportDebug,
+  rejectionHandlers: transportDebug,
+  exitOnError: false,
 });
 
 export default {
-    info: info,
-    debug: debug,
-    meta: meta,
+  info: info,
+  debug: debug,
+  meta: meta,
 };
