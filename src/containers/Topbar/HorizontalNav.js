77cb679cedd4c10a18bba2a51e6029d3c8de6fb0
DFST <mk@dfst.io>
2024-05-15 19:04:55 +0300

    advanced 19

--- src/containers/Topbar/HorizontalNav.js
+++ src/containers/Topbar/HorizontalNav.js
@@ -62,7 +62,7 @@ const HorizontalNav = () => {
         </Link>
       </Menu.Item>
       <SubMenu
-        popupClassName="gx-menu-horizontal-flex gx-submenu-popup-curve"
+        popupClassName="gx-menu-horizontal"
         key="advanced"
         title={<IntlMessages id="sidebar.advanced" />}
       >
