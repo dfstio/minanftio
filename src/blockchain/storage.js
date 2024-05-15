fde5828c45643dcb5af6f07935a7b692738e4593
DFST <mk@dfst.io>
2024-05-15 15:37:58 +0300

    image loading

--- src/blockchain/storage.js
+++ src/blockchain/storage.js
@@ -2,7 +2,7 @@ export function storageUrl(storage, big = false, nocache = false) {
   const cacheUrl = "https://res.cloudinary.com/minanft/image/fetch/";
   const cacheSmallUrl =
     "https://res.cloudinary.com/minanft/image/fetch/h_300,q_100,f_auto/";
-  const ipfsUrl = "https://gateway.pinata.cloud/ipfs/";
+  const ipfsUrl = "https://salmon-effective-amphibian-898.mypinata.cloud/ipfs/";
   const arweaveUrl = "https://arweave.net/";
 
   if (storage.length === 0) {
@@ -17,6 +17,24 @@ export function storageUrl(storage, big = false, nocache = false) {
     return storage;
   }
 
+  if (nocache === true) return url;
+  if (big === true) return cacheUrl + url;
+  return cacheSmallUrl + url;
+}
+
+export function storageUrlFromURL(fullUrl, big = false, nocache = false) {
+  if (fullUrl === undefined) {
+    console.error("Empty fullUrl");
+    return "";
+  }
+  if (fullUrl.includes("https://gateway.pinata.cloud/ipfs/")) {
+    const storage = fullUrl.replace("https://gateway.pinata.cloud/ipfs/", "i:");
+    return storageUrl(storage, big, nocache);
+  }
+  const url = fullUrl;
+  const cacheUrl = "https://res.cloudinary.com/minanft/image/fetch/";
+  const cacheSmallUrl =
+    "https://res.cloudinary.com/minanft/image/fetch/h_300,q_100,f_auto/";
   if (nocache === true) return url;
   if (big === true) return cacheUrl + url;
   return cacheSmallUrl + url;
