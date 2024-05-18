6c69471f9550bde96c2cce38c932d7d878a3a384
DFST <mk@dfst.io>
2024-05-19 00:45:45 +0300

    tx

---
+++ src/routes/Tx/index.js
@@ -0,0 +1,166 @@
+/* eslint-disable react/jsx-no-target-blank */
+import React, { useState, useEffect } from "react";
+import algoliasearch from "algoliasearch";
+import {
+  Button,
+  message,
+  Row,
+  Col,
+  Form,
+  Input,
+  Radio,
+  Card,
+  Upload,
+  Select,
+  Table,
+  Divider,
+  Descriptions,
+} from "antd";
+
+import logger from "../../serverless/logger";
+import { set } from "nprogress";
+const { REACT_APP_ALGOLIA_KEY, REACT_APP_ALGOLIA_PROJECT } = process.env;
+const searchClient = algoliasearch(
+  REACT_APP_ALGOLIA_PROJECT,
+  REACT_APP_ALGOLIA_KEY
+);
+const rollupTxs = searchClient.initIndex("rollup-txs");
+const rollupBlocks = searchClient.initIndex("rollup-blocks");
+
+const logm = logger.info.child({ winstonModule: "Tx" });
+const { REACT_APP_DEBUG } = process.env;
+
+const { TextArea } = Input;
+const RadioButton = Radio.Button;
+const RadioGroup = Radio.Group;
+
+const DEBUG = "true" === process.env.REACT_APP_DEBUG;
+
+const Tx = ({ match }) => {
+  const [tx, setTx] = useState({});
+  const [block, setBlock] = useState({});
+  const [txLoaded, setTxLoaded] = useState(false);
+  const [blockLoaded, setBlockLoaded] = useState(false);
+  const [messageText, setMessageText] = useState("Loading tx data");
+  const [form] = Form.useForm();
+
+  const log = logm.child({ winstonComponent: "Faucet" });
+
+  if (DEBUG) console.log("txId", match.params.txId, "match", match);
+  if (DEBUG) console.log("Tx", tx);
+
+  useEffect(() => {
+    async function getItem() {
+      if (match.params.txId !== undefined) {
+        try {
+          const tx = await rollupTxs.getObject(match.params.txId);
+          if (DEBUG) console.log("Tx received", tx);
+          if (tx !== undefined) {
+            setTx(tx);
+            setTxLoaded(true);
+          } else setMessageText("Tx not found");
+          if (
+            tx.chain !== undefined &&
+            tx.contractAddress !== undefined &&
+            tx.blockNumber !== undefined
+          ) {
+            try {
+              const objectID =
+                tx.chain +
+                "." +
+                tx.contractAddress +
+                "." +
+                tx.blockNumber.toString();
+              const block = await rollupBlocks.getObject(objectID);
+              if (DEBUG) console.log("Block received", block);
+              if (block !== undefined) {
+                setBlock(block);
+                setBlockLoaded(true);
+              } else setMessageText("Block not found");
+            } catch (error) {
+              console.log("Block not received", error);
+              setMessageText("Block not found");
+            }
+          } else console.error("Tx object has wrong format", tx);
+          setTx(tx);
+        } catch (error) {
+          console.log("Tx not received", error);
+          setMessageText("Tx not found");
+        }
+      }
+    }
+    getItem();
+  }, [match]);
+
+  return (
+    <>
+      <div className="gx-main-content">
+        <Row>
+          <Col xxl={24} xl={24} lg={24} md={24} sm={24} xs={24}>
+            <Card className="gx-card" key="faucetCard" title="Transaction">
+              <Form
+                form={form}
+                key="txForm"
+                labelCol={{
+                  span: 24,
+                }}
+                wrapperCol={{
+                  span: 24,
+                }}
+                layout="horizontal"
+                initialValues={{ publicKey: "", chain: "zeko" }}
+              >
+                <div>
+                  <Row>
+                    <Col xxl={12} xl={12} lg={14} md={24} sm={24} xs={24}>
+                      <Form.Item
+                        label="Transaction data"
+                        name="txData"
+                        placeholder=""
+                      >
+                        {txLoaded ? (
+                          <Descriptions bordered>
+                            {Object.keys(tx).map((key) => (
+                              <Descriptions.Item label={key}>
+                                {typeof tx[key] === "object"
+                                  ? JSON.stringify(tx[key], null, 2)
+                                  : tx[key]}
+                              </Descriptions.Item>
+                            ))}
+                          </Descriptions>
+                        ) : (
+                          <p>{messageText}</p>
+                        )}
+                      </Form.Item>
+                      <Form.Item
+                        label="Block data"
+                        name="blockData"
+                        placeholder=""
+                      >
+                        {blockLoaded ? (
+                          <Descriptions bordered>
+                            {Object.keys(block).map((key) => (
+                              <Descriptions.Item label={key}>
+                                {typeof tx[key] === "object"
+                                  ? JSON.stringify(tx[key], null, 2)
+                                  : tx[key]}
+                              </Descriptions.Item>
+                            ))}
+                          </Descriptions>
+                        ) : (
+                          <p>{messageText}</p>
+                        )}
+                      </Form.Item>
+                    </Col>
+                  </Row>
+                </div>
+              </Form>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
+};
+
+export default Tx;
