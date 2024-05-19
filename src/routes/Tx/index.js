79047dfd206e43010edb809e4b633f34a496d239
DFST <mk@dfst.io>
2024-05-19 05:15:31 +0300

    style

--- src/routes/Tx/index.js
+++ src/routes/Tx/index.js
@@ -150,7 +150,7 @@ const Tx = ({ match }) => {
                 layout="horizontal"
                 initialValues={{ publicKey: "", chain: "zeko" }}
               >
-                <div style={{ position: "relative" }}>
+                <div className="gx-mt-2">
                   <Row>
                     <Col xxl={24} xl={24} lg={24} md={24} sm={24} xs={24}>
                       <Form.Item
