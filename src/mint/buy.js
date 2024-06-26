048a528e17b86cc5c0a8098d9aa6c91bf6b77d7e
DFST <mk@dfst.io>
2024-06-26 15:52:16 +0400

    buy 1

---
+++ src/mint/buy.js
@@ -0,0 +1,139 @@
+import { serializeTransaction } from "./transaction";
+import { sendBuyTransaction } from "./send";
+import { PublicKey, UInt64, Mina } from "o1js";
+import {
+  MinaNFT,
+  NameContractV2,
+  initBlockchain,
+  MINANFT_NAME_SERVICE_V2,
+  fetchMinaAccount,
+  serializeFields,
+  BuyParams,
+} from "minanft";
+import { chainId } from "../blockchain/explorer";
+
+export async function buyNFT(params) {
+  console.time("ready to sign");
+  console.log("Buy NFT", params);
+
+  const { price, buyer, name } = params;
+
+  const chain = chainId();
+
+  if (buyer === undefined) {
+    console.error("Buyer address is undefined");
+    return {
+      success: false,
+      error: "Buyer address is undefined",
+    };
+  }
+
+  if (name === undefined || name === "") {
+    console.error("NFT name is undefined");
+    return {
+      success: false,
+      error: "NFT name is undefined",
+    };
+  }
+
+  const contractAddress = MINANFT_NAME_SERVICE_V2;
+  if (contractAddress === undefined) {
+    console.error("Contract address is undefined");
+    return {
+      success: false,
+      error: "Contract address is undefined",
+    };
+  }
+
+  if (chain === undefined) {
+    console.error("Chain is undefined");
+    return {
+      success: false,
+      error: "Chain is undefined",
+    };
+  }
+
+  console.time("prepared data");
+  console.log("contractAddress", contractAddress);
+
+  const address = PublicKey.fromBase58(params.address);
+  const net = await initBlockchain(chain);
+  console.log("network id", Mina.getNetworkId());
+  const sender = PublicKey.fromBase58(buyer);
+
+  console.timeEnd("prepared data");
+
+  const zkAppAddress = PublicKey.fromBase58(MINANFT_NAME_SERVICE_V2);
+  const zkApp = new NameContractV2(zkAppAddress);
+  const tokenId = zkApp.deriveTokenId();
+  const fee = Number((await MinaNFT.fee()).toBigInt());
+  const memo = ("buy NFT @" + name).substring(0, 30);
+  await fetchMinaAccount({ publicKey: sender });
+  await fetchMinaAccount({ publicKey: zkAppAddress });
+  await fetchMinaAccount({ publicKey: address, tokenId });
+  console.time("prepared tx");
+
+  /*
+      export class SellParams extends Struct({
+        address: PublicKey,
+        price: UInt64,
+      }) {}
+  */
+
+  const buyParams = new BuyParams({
+    address,
+    price: UInt64.from(price),
+  });
+
+  const tx = await Mina.transaction({ sender, fee, memo }, async () => {
+    await zkApp.buy(buyParams);
+  });
+
+  const serializedTransaction = serializeTransaction(tx);
+  const transaction = tx.toJSON();
+  console.log("Transaction", tx.toPretty());
+  const payload = {
+    transaction,
+    onlySign: true,
+    feePayer: {
+      fee: fee,
+      memo: memo,
+    },
+  };
+  console.timeEnd("prepared tx");
+  console.timeEnd("ready to sign");
+  const txResult = await window.mina?.sendTransaction(payload);
+  console.log("Transaction result", txResult);
+  console.time("sent transaction");
+  const signedData = txResult?.signedData;
+  if (signedData === undefined) {
+    console.log("No signed data");
+    return {
+      success: false,
+      error: "No user signature",
+    };
+  }
+
+  const jobId = await sendBuyTransaction({
+    name,
+    serializedTransaction,
+    signedData,
+    buyParams: serializeFields(BuyParams.toFields(buyParams)),
+    contractAddress,
+    chain,
+  });
+  console.timeEnd("sent transaction");
+  console.log("Sent transaction, jobId", jobId);
+  if (jobId === undefined) {
+    console.error("JobId is undefined");
+    return {
+      success: false,
+      error: "JobId is undefined",
+    };
+  }
+
+  return {
+    success: true,
+    jobId,
+  };
+}
--- src/routes/Explore/Buy.js
+++ src/routes/Explore/Buy.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateAddress } from "../../appRedux/actions";
 import { minaLogin } from "../../blockchain/mina";
 import IntlMessages from "util/IntlMessages";
+import { buyNFT } from "../../mint/buy";
+import { waitForTransaction } from "../../mint/send";
 
 import logger from "../../serverless/logger";
 const logm = logger.info.child({
@@ -19,8 +21,24 @@ const BuyButton = ({ item }) => {
 
   const log = logm.child({ wf: "BuyButton", item, address });
 
+  async function buy() {
+    if (DEBUG) console.log("SellButton onClick", item);
+    const newAddress = await minaLogin();
+    dispatch(updateAddress(newAddress));
+    let buyResult = await buyNFT({
+      name: item.name,
+      price: Number(item.price),
+      buyer: newAddress,
+      address: item.address,
+    });
+    console.log("BuyButton buyResult", buyResult);
+    const jobId = buyResult.jobId;
+    buyResult = await waitForTransaction(jobId);
+    console.log("BuyButton tx buyResult", buyResult);
+  }
+
   return (
-    <Button type="primary">
+    <Button type="primary" onClick={buy}>
       <IntlMessages id="sidebar.algolia.buy" />
     </Button>
   );
