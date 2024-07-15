567f86fed0b449b368b5a69ae5250a0cc015beb6
DFST <mk@dfst.io>
2024-07-15 12:38:03 +0400

    gateway

--- src/blockchain/storage.js
+++ src/blockchain/storage.js
@@ -33,6 +33,13 @@ export function storageUrlFromURL(url) {
     console.error("Empty fullUrl");
     return "";
   }
+  if (
+    url ===
+    "https://gateway.pinata.cloud/ipfs/bafybeidt3j3jmsh5oyhzbr6ylpn3xjbrs2xrigmpyppe4s7xqu77srdyai"
+  ) {
+    console.error("handling special case for pinata gateway");
+    return url;
+  }
   if (url.includes("https://gateway.pinata.cloud/ipfs/")) {
     return (
       cacheSmallUrl +
